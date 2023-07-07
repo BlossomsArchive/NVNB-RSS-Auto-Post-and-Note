0bf8c852e905549cc666eb9b50cb1e7a303c1135
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-07 17:44:58 +0900

    Update nvnb-ipa.py

--- nvnb-ipa.py
+++ nvnb-ipa.py
@@ -7,14 +7,12 @@ old_up = f.read()
 f.close()
 
 entries = feedparser.parse('https://www.ipa.go.jp/security/alert-rss.rdf')['entries']
-print(entries[0]["updated"])
-print(old_up)
 i = 0
 
 while (True):
     now_entry = entries[i]
-    if now_entry['date'] == old_up:
-        new_up = entries[0]['date']
+    if now_entry["updated"] == old_up:
+        new_up = entries[0]["updated"]
         g = open("nvnb-ipa.txt", "w")
         g.write(new_up)
         g.close
