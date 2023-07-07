1e00749da60f36e85d504fb348e67c27fed3e43f
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-07 18:11:01 +0900

    Update nvnb-ipa.py

--- nvnb-ipa.py
+++ nvnb-ipa.py
@@ -9,18 +9,23 @@ f.close()
 entries = feedparser.parse('https://www.ipa.go.jp/security/alert-rss.rdf')['entries']
 i = 0
 
-while (True):
-    if entries[i]["updated"] == old_up:
-        new_up = entries[0]["updated"]
-        g = open("nvnb-ipa.txt", "w")
-        g.write(new_up)
-        g.close
-        break
-    else:
-        title = entries[i]['title']
-        page_url = entries[i]['link']
-        
-        post_text ="【IPA】\n" +title + "\n" + page_url + "\n\nその他の情報はこちら\nhttps://nvnb.blossomsarchive.com/"
-        print(post_text+"\n")
-    print(entries[i]["updated"])
-    i = i+1
+try:
+    while (True):
+        if entries[i]["updated"] == old_up:
+            new_up = entries[0]["updated"]
+            g = open("nvnb-ipa.txt", "w")
+            g.write(new_up)
+            g.close
+            break
+        else:
+            title = entries[i]['title']
+            page_url = entries[i]['link']
+            
+            post_text ="【IPA】\n" +title + "\n" + page_url + "\n\nその他の情報はこちら\nhttps://nvnb.blossomsarchive.com/"
+            print(post_text+"\n")
+        print(entries[i]["updated"])
+        i = i+1
+except:
+    g = open("nvnb-ipa.txt", "w")
+    g.write(new_up)
+    g.close
