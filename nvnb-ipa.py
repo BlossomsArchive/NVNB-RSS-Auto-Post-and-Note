47ecda1b8f1c5dc912f0a93ed39acdfd84c3e483
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-07 17:50:39 +0900

    Update nvnb-ipa.py

--- nvnb-ipa.py
+++ nvnb-ipa.py
@@ -13,16 +13,15 @@ print(old_up)
 print(entries[0]["updated"])
 
 while (True):
-    now_entry = entries[i]
-    if now_entry["updated"] == old_up:
+    if entries[i]["updated"] == old_up:
         new_up = entries[0]["updated"]
         g = open("nvnb-ipa.txt", "w")
         g.write(new_up)
         g.close
         break
     else:
-        title = now_entry['title']
-        page_url = now_entry['link']
+        title = entries[i]['title']
+        page_url = entries[i]['link']
         
         post_text ="【IPA】\n" +title + "\n" + page_url + "\n\nその他の情報はこちら\nhttps://nvnb.blossomsarchive.com/"
         print(post_text+"\n")
