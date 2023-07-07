396a90a1ea991162cc7abce619a4a4de9030ef44
瑞井 箕月 <105696149+MizuiMiduki@users.noreply.github.com>
2023-07-07 18:44:00 +0900

    Update nvnb-ipa.py

--- nvnb-ipa.py
+++ nvnb-ipa.py
@@ -9,11 +9,6 @@ try:
     
     entries = feedparser.parse('https://www.ipa.go.jp/security/alert-rss.rdf')['entries']
     i = 0
-
-    print("old:"+old_up)
-    print(type(old_up))
-    print("new:"+entries[0]["updated"])
-    print(type(entries[0]["updated"]))
     
     feed_size = len(entries)
     
@@ -28,10 +23,33 @@ try:
         else:
             title = entries[i]['title']
             page_url = entries[i]['link']
+            # 送信する記事データ
+            post_data = {
+                'title': "[IPA] "+title,
+                'content': "<p>IPAの記事リンク</p>"+"<a href= \""+page_url+"\">"+page_url+"</a>",
+                'categories': '9',
+                'status': 'publish',  # draft=下書き、publish=公開　省略時はdraftになる
+                'featured_media':163,
+            }
+            # Post APIのURL
+            post_api_url = f'{blog_url}/wp-json/wp/v2/posts'
+    
+            # 記事投稿リクエスト
+            #response = requests.post(post_api_url, json=post_data, auth=(api_user, api_password))
             
             post_text ="【IPA】\n" +title + "\n" + page_url + "\n\nその他の情報はこちら\nhttps://nvnb.blossomsarchive.com/"
             print(post_text+"\n")
-        print(i)
+                try:
+                    #SNS投稿API
+                    # Misskey
+                    misskey_address = os.environ.get("MISSKEY_SERVER_ADDRESS")
+                    misskey_token = os.environ.get("MISSKEY_TOKEN")
+                    api = Misskey(misskey_address)
+                    api.token = misskey_token
+                    #api.notes_create(text=post_text)
+                    time.sleep(10)
+                except:
+                    pass
         i = i+1
 except:
     new_up = entries[0]["updated"]
